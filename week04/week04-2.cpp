195431fe3be1279f420042af0b322c287a2802e9
王炫晟 <91250441+happy123903@users.noreply.github.com>
2021-10-07 17:39:00 +0800

    Add files via upload

---
+++ week04/week04-2.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    float a=1.1254684894456231;
+    double b=1.1254684894456231;
+    printf("%f\n",a);
+    printf("%f\n",b);
+    printf("%.4f\n",b);
+    printf("%.9f\n",b);
+    printf("%.13f\n",b);
+}
