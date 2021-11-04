ba86d157f6065f9f608a3e3252b85c2181ff704b
王炫晟 <91250441+happy123903@users.noreply.github.com>
2021-11-04 17:32:21 +0800

    Add files via upload

---
+++ Week08/week08-3.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int n,bad=0;
+    scanf("%d",&n);
+    for(int i=2; i<n; i++)
+    {
+        if(n%i==0) bad++;
+    }
+    if(bad==0) printf("%d是質數",n);
+    else printf("%d不是質數",n);
+}
